d2445252c91159a0770d8ee6b65bd72c6ce9f47b
Ben Schlussel <benschlussel@Bens-MacBook-Air.local>
2026-02-04 16:02:20 -0500

    feat: Slack import improvements
    - Update Slack import API route
    - Enhance Slack import modal component
    - Update team sidebar

--- apps/user-web/src/components/team/team-sidebar.tsx
+++ apps/user-web/src/components/team/team-sidebar.tsx
@@ -69,8 +69,8 @@ export function TeamSidebar({
   const pathname = usePathname()
 
   return (
-    <div className="w-64 shrink-0 border-r bg-muted/30 flex flex-col">
-      <ScrollArea className="flex-1">
+    <div className="w-64 h-full shrink-0 border-r bg-muted/30 flex flex-col overflow-hidden">
+      <ScrollArea className="flex-1 min-h-0">
         <div className="p-2">
           {/* Channels Section */}
           <Collapsible open={channelsOpen} onOpenChange={setChannelsOpen} suppressHydrationWarning>
